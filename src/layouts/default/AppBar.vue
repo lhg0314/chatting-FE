0b3e630494f12b54b123d143271b13b060d6a70a
윤지원 <yunji1@openobject.net>
2024-02-02 09:23:47 +0000

    로그아웃기능 구현

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,9 +4,23 @@
       <v-icon icon="mdi mdi-chat" />
       Chatting
     </v-app-bar-title>
+    <template v-slot:append>
+      <v-btn v-if="store.getIsMember().value" variant="outlined" @click="logout">로그아웃</v-btn>
+    </template>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-//
+import { clearTokenInfo } from "@/axios/apiUtil"
+import { useAppStore } from "@/store/comm"
+import { useRouter } from "vue-router"
+const store = useAppStore()
+const router = useRouter()
+
+const logout = () => {
+  clearTokenInfo()
+  router.push({
+    name: "/signIn"
+  })
+}
 </script>
